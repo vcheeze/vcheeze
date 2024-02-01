678169bdb95c8eddbeeb67fe0fdb88d695133073
Peter Chen <manchesterutd7@gmail.com>
2024-02-01 10:52:57 +0400

    feat: add side navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,10 @@
-<script lang="ts">
-  import { onMount } from 'svelte';
-  import SunIcon from 'virtual:icons/radix-icons/sun';
-  import MoonIcon from 'virtual:icons/radix-icons/moon';
-  import { THEMES } from '$lib/config';
+<script>
+  import Nav from './Nav.svelte';
   import '../styles/global.scss';
-
-  let currentTheme: string | null;
-
-  onMount(() => {
-    currentTheme = document.body.getAttribute('data-theme');
-  });
-
-  function setTheme(theme: string) {
-    if (!Object.values(THEMES).includes(theme)) return;
-    localStorage.setItem('theme', theme);
-    document.body.setAttribute('data-theme', theme);
-  }
-
-  function toggleTheme() {
-    const theme = currentTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
-    currentTheme = theme;
-    setTheme(theme);
-  }
 </script>
 
-<button class="theme-switch" on:click={toggleTheme}>
-  {#if currentTheme === THEMES.LIGHT}
-    <MoonIcon />
-  {:else}
-    <SunIcon />
-  {/if}
-</button>
+<Nav />
+
 <div class="content">
   <slot />
 </div>
@@ -38,19 +12,6 @@
 <style lang="scss">
   @import '../styles/variables.scss';
 
-  .theme-switch {
-    all: unset;
-    position: fixed;
-    top: 1rem;
-    right: 1.5rem;
-    display: flex;
-    padding: 0.5rem;
-    border-radius: 0.25rem;
-    &:hover {
-      cursor: pointer;
-      background-color: var(--button-hover-background-color);
-    }
-  }
   .content {
     padding: 6rem;
     margin: 0 auto;
--- src/routes/blog/Card.svelte
+++ src/routes/blog/Card.svelte
@@ -1,22 +1,41 @@
 <script lang="ts">
+  import { formatDate } from '$lib/utils';
+
   export let title: string | undefined;
   export let link: string;
+  export let date: string;
 </script>
 
 <a class="card" href={link}>
-  {title}
+  <div class="card__title">{title}</div>
+  <div class="card__date">{formatDate(date)}</div>
 </a>
 
 <style lang="scss">
   .card {
-    display: block;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 0.5rem;
     background-color: var(--card-background-color);
     border-radius: 0.5rem;
     padding: 2rem;
+    color: var(--foreground-color);
     text-decoration: none;
-    transition: background-color 0.5s;
+    transition: background-color 0.5s ease-out;
+    & > div {
+      transition: all 0.25s ease-in-out;
+    }
     &:hover {
       background-color: var(--card-hover-background-color);
+      & > div {
+        transform: translateY(-0.5rem);
+      }
+    }
+
+    &__date {
+      font-size: 0.75rem;
+      color: var(--card-description-color);
     }
   }
 </style>
